79306ef90a8215c7e4b45240f2a8651c94f97980
Joe Potentier <joseph.potentier@gmail.com>
2020-01-17 12:07:33 -0700

    fixed licenses

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 #  coding: utf-8
 
-# Copyright 2013 Abram Hindle, Eddie Antonio Santos, Joe Potentier
+# Copyright 2020 Abram Hindle, Eddie Antonio Santos, Joseph Potentier
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
